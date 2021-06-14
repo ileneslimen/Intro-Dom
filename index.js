28099252bdc815cdc87116b24e034c4a50e0c5bd
ileneslimen <slimennorhen1995@gmail.com>
2021-06-14 15:43:15 +0100

    introdom

---
+++ index.js
@@ -0,0 +1,42 @@
+// var title=document.querySelector('h1')
+// title.style.color='red'
+
+
+// var main= document.querySelector('#main')
+// var main =document.getElementById('main')
+// console.log(main)
+
+// var para=document.querySelector('.para').innerHTML
+// var par=document.querySelector('.para').innerText
+// console.log(para)
+// console.log(par)
+
+// var element=document.createElement('h1')
+// element.innerText='Hello World'
+// main.appendChild(element)
+
+// var name=''
+
+// function SayHello(){
+// if (name) alert('hello'+ name)
+// else alert ('enter a name')
+// }
+// function getName(){
+//     var input =document.getElementById('fname')
+//     name=input.value
+// }
+
+// var btnPlus= document.querySelectorAll('.btnplus')
+
+// for (let i=0; i<btnPlus.length;i++  ){
+//     btnPlus[i].addEventListener('click', function(){
+//         btnPlus[i].nextElementSibling.innerHTML++
+//     })
+// }
+
+
+// var btnMinus=document.querySelectorAll('.btnminus')
+// btnMinus.addEventListener('click',function(){
+//     if(btnMinus.previousElementSibling.innerHTML>0)
+//     btnMinus.previousElementSibling.innerHTML--
+// })
